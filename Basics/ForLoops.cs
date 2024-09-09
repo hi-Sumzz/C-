4dd98bbd5dbfa2512771290494008a872bd035cc
Sumzz <lokiinlockup@gmail.com>
2024-09-09 07:49:05 +0530

    C# practice

---
+++ Basics/ForLoops.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Basics
+{
+    class ForLoops
+    {
+        public static void Main()
+        {
+            int[] marks = new int[5] { 99, 98, 92, 97, 95 };
+            string[] yes =  { "sumeet", "hiremath" };
+            int b = 20;
+            for (int a=10 ; a < b; a++)
+            {
+                Console.WriteLine("a="+a);
+
+            }
+            Console.WriteLine("-------------------------");
+            foreach (int i  in marks)
+            {
+                Console.WriteLine("i="+i);
+            }
+            Console.WriteLine("-------------------------");
+            foreach (string i in yes)
+            {
+                Console.WriteLine(i);
+            }
+            
+            
+            
+
+            
+
+        }
+       
+    }
+}
---
+++ Basics/IfStatements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Basics
+{
+    class IfStatements
+    {
+        static void Main()
+        {
+            int a = 20;
+            int b = 20;
+            if (a == b)
+            {
+                Console.WriteLine("a is equal to b");
+            }
+        }
+    }
+}
